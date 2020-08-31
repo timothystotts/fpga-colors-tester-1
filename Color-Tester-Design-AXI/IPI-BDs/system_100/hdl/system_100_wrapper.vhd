f5ca2397907bcadee7528ab713b7d9338ca95205
Timothy Stotts <timothystotts08@gmail.com>
2020-08-31 12:58:05 -0400

    First mostly working draft of color mixing application that inputs LED index and 24-bit color palette value on the Pmod KYPD, and then updates the color of the specified LED and a text line on the Pmod OLEDrgb.

---
+++ Color-Tester-Design-AXI/IPI-BDs/system_100/hdl/system_100_wrapper.vhd
@@ -0,0 +1,532 @@
+--Copyright 1986-2019 Xilinx, Inc. All Rights Reserved.
+----------------------------------------------------------------------------------
+--Tool Version: Vivado v.2019.1 (lin64) Build 2552052 Fri May 24 14:47:09 MDT 2019
+--Date        : Fri Aug 28 22:48:26 2020
+--Host        : l2study running 64-bit Ubuntu 18.04.5 LTS
+--Command     : generate_target system_100_wrapper.bd
+--Design      : system_100_wrapper
+--Purpose     : IP block netlist
+----------------------------------------------------------------------------------
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+library UNISIM;
+use UNISIM.VCOMPONENTS.ALL;
+entity system_100_wrapper is
+  port (
+    ddr3_sdram_addr : out STD_LOGIC_VECTOR ( 13 downto 0 );
+    ddr3_sdram_ba : out STD_LOGIC_VECTOR ( 2 downto 0 );
+    ddr3_sdram_cas_n : out STD_LOGIC;
+    ddr3_sdram_ck_n : out STD_LOGIC_VECTOR ( 0 to 0 );
+    ddr3_sdram_ck_p : out STD_LOGIC_VECTOR ( 0 to 0 );
+    ddr3_sdram_cke : out STD_LOGIC_VECTOR ( 0 to 0 );
+    ddr3_sdram_cs_n : out STD_LOGIC_VECTOR ( 0 to 0 );
+    ddr3_sdram_dm : out STD_LOGIC_VECTOR ( 1 downto 0 );
+    ddr3_sdram_dq : inout STD_LOGIC_VECTOR ( 15 downto 0 );
+    ddr3_sdram_dqs_n : inout STD_LOGIC_VECTOR ( 1 downto 0 );
+    ddr3_sdram_dqs_p : inout STD_LOGIC_VECTOR ( 1 downto 0 );
+    ddr3_sdram_odt : out STD_LOGIC_VECTOR ( 0 to 0 );
+    ddr3_sdram_ras_n : out STD_LOGIC;
+    ddr3_sdram_reset_n : out STD_LOGIC;
+    ddr3_sdram_we_n : out STD_LOGIC;
+    dip_switches_4bits_tri_i : in STD_LOGIC_VECTOR ( 3 downto 0 );
+    eth_mdio_mdc_mdc : out STD_LOGIC;
+    eth_mdio_mdc_mdio_io : inout STD_LOGIC;
+    eth_mii_col : in STD_LOGIC;
+    eth_mii_crs : in STD_LOGIC;
+    eth_mii_rst_n : out STD_LOGIC;
+    eth_mii_rx_clk : in STD_LOGIC;
+    eth_mii_rx_dv : in STD_LOGIC;
+    eth_mii_rx_er : in STD_LOGIC;
+    eth_mii_rxd : in STD_LOGIC_VECTOR ( 3 downto 0 );
+    eth_mii_tx_clk : in STD_LOGIC;
+    eth_mii_tx_en : out STD_LOGIC;
+    eth_mii_txd : out STD_LOGIC_VECTOR ( 3 downto 0 );
+    eth_ref_clk : out STD_LOGIC;
+    ja_pin10_io : inout STD_LOGIC;
+    ja_pin1_io : inout STD_LOGIC;
+    ja_pin2_io : inout STD_LOGIC;
+    ja_pin3_io : inout STD_LOGIC;
+    ja_pin4_io : inout STD_LOGIC;
+    ja_pin7_io : inout STD_LOGIC;
+    ja_pin8_io : inout STD_LOGIC;
+    ja_pin9_io : inout STD_LOGIC;
+    jb_pin10_io : inout STD_LOGIC;
+    jb_pin1_io : inout STD_LOGIC;
+    jb_pin2_io : inout STD_LOGIC;
+    jb_pin3_io : inout STD_LOGIC;
+    jb_pin4_io : inout STD_LOGIC;
+    jb_pin7_io : inout STD_LOGIC;
+    jb_pin8_io : inout STD_LOGIC;
+    jb_pin9_io : inout STD_LOGIC;
+    led03_rgb : out STD_LOGIC_VECTOR ( 11 downto 0 );
+    led47 : out STD_LOGIC_VECTOR ( 3 downto 0 );
+    push_buttons_4bits_tri_i : in STD_LOGIC_VECTOR ( 3 downto 0 );
+    qspi_flash_io0_io : inout STD_LOGIC;
+    qspi_flash_io1_io : inout STD_LOGIC;
+    qspi_flash_io2_io : inout STD_LOGIC;
+    qspi_flash_io3_io : inout STD_LOGIC;
+    qspi_flash_sck_io : inout STD_LOGIC;
+    qspi_flash_ss_io : inout STD_LOGIC;
+    reset : in STD_LOGIC;
+    sys_clock : in STD_LOGIC;
+    usb_uart_rxd : in STD_LOGIC;
+    usb_uart_txd : out STD_LOGIC
+  );
+end system_100_wrapper;
+
+architecture STRUCTURE of system_100_wrapper is
+  component system_100 is
+  port (
+    sys_clock : in STD_LOGIC;
+    ddr3_sdram_dq : inout STD_LOGIC_VECTOR ( 15 downto 0 );
+    ddr3_sdram_dqs_p : inout STD_LOGIC_VECTOR ( 1 downto 0 );
+    ddr3_sdram_dqs_n : inout STD_LOGIC_VECTOR ( 1 downto 0 );
+    ddr3_sdram_addr : out STD_LOGIC_VECTOR ( 13 downto 0 );
+    ddr3_sdram_ba : out STD_LOGIC_VECTOR ( 2 downto 0 );
+    ddr3_sdram_ras_n : out STD_LOGIC;
+    ddr3_sdram_cas_n : out STD_LOGIC;
+    ddr3_sdram_we_n : out STD_LOGIC;
+    ddr3_sdram_reset_n : out STD_LOGIC;
+    ddr3_sdram_ck_p : out STD_LOGIC_VECTOR ( 0 to 0 );
+    ddr3_sdram_ck_n : out STD_LOGIC_VECTOR ( 0 to 0 );
+    ddr3_sdram_cke : out STD_LOGIC_VECTOR ( 0 to 0 );
+    ddr3_sdram_cs_n : out STD_LOGIC_VECTOR ( 0 to 0 );
+    ddr3_sdram_dm : out STD_LOGIC_VECTOR ( 1 downto 0 );
+    ddr3_sdram_odt : out STD_LOGIC_VECTOR ( 0 to 0 );
+    reset : in STD_LOGIC;
+    usb_uart_rxd : in STD_LOGIC;
+    usb_uart_txd : out STD_LOGIC;
+    eth_mii_col : in STD_LOGIC;
+    eth_mii_crs : in STD_LOGIC;
+    eth_mii_rst_n : out STD_LOGIC;
+    eth_mii_rx_clk : in STD_LOGIC;
+    eth_mii_rx_dv : in STD_LOGIC;
+    eth_mii_rx_er : in STD_LOGIC;
+    eth_mii_rxd : in STD_LOGIC_VECTOR ( 3 downto 0 );
+    eth_mii_tx_clk : in STD_LOGIC;
+    eth_mii_tx_en : out STD_LOGIC;
+    eth_mii_txd : out STD_LOGIC_VECTOR ( 3 downto 0 );
+    eth_mdio_mdc_mdc : out STD_LOGIC;
+    eth_mdio_mdc_mdio_i : in STD_LOGIC;
+    eth_mdio_mdc_mdio_o : out STD_LOGIC;
+    eth_mdio_mdc_mdio_t : out STD_LOGIC;
+    eth_ref_clk : out STD_LOGIC;
+    dip_switches_4bits_tri_i : in STD_LOGIC_VECTOR ( 3 downto 0 );
+    push_buttons_4bits_tri_i : in STD_LOGIC_VECTOR ( 3 downto 0 );
+    led03_rgb : out STD_LOGIC_VECTOR ( 11 downto 0 );
+    led47 : out STD_LOGIC_VECTOR ( 3 downto 0 );
+    ja_pin1_o : out STD_LOGIC;
+    ja_pin7_i : in STD_LOGIC;
+    ja_pin2_o : out STD_LOGIC;
+    ja_pin8_i : in STD_LOGIC;
+    ja_pin3_o : out STD_LOGIC;
+    ja_pin9_i : in STD_LOGIC;
+    ja_pin10_o : out STD_LOGIC;
+    ja_pin4_o : out STD_LOGIC;
+    ja_pin3_i : in STD_LOGIC;
+    ja_pin4_i : in STD_LOGIC;
+    ja_pin1_i : in STD_LOGIC;
+    ja_pin2_i : in STD_LOGIC;
+    ja_pin10_t : out STD_LOGIC;
+    ja_pin8_t : out STD_LOGIC;
+    ja_pin9_t : out STD_LOGIC;
+    ja_pin4_t : out STD_LOGIC;
+    ja_pin9_o : out STD_LOGIC;
+    ja_pin10_i : in STD_LOGIC;
+    ja_pin7_t : out STD_LOGIC;
+    ja_pin1_t : out STD_LOGIC;
+    ja_pin2_t : out STD_LOGIC;
+    ja_pin7_o : out STD_LOGIC;
+    ja_pin3_t : out STD_LOGIC;
+    ja_pin8_o : out STD_LOGIC;
+    jb_pin1_o : out STD_LOGIC;
+    jb_pin7_i : in STD_LOGIC;
+    jb_pin2_o : out STD_LOGIC;
+    jb_pin8_i : in STD_LOGIC;
+    jb_pin3_o : out STD_LOGIC;
+    jb_pin9_i : in STD_LOGIC;
+    jb_pin10_o : out STD_LOGIC;
+    jb_pin4_o : out STD_LOGIC;
+    jb_pin3_i : in STD_LOGIC;
+    jb_pin4_i : in STD_LOGIC;
+    jb_pin1_i : in STD_LOGIC;
+    jb_pin2_i : in STD_LOGIC;
+    jb_pin10_t : out STD_LOGIC;
+    jb_pin8_t : out STD_LOGIC;
+    jb_pin9_t : out STD_LOGIC;
+    jb_pin4_t : out STD_LOGIC;
+    jb_pin9_o : out STD_LOGIC;
+    jb_pin10_i : in STD_LOGIC;
+    jb_pin7_t : out STD_LOGIC;
+    jb_pin1_t : out STD_LOGIC;
+    jb_pin2_t : out STD_LOGIC;
+    jb_pin7_o : out STD_LOGIC;
+    jb_pin3_t : out STD_LOGIC;
+    jb_pin8_o : out STD_LOGIC;
+    qspi_flash_io0_i : in STD_LOGIC;
+    qspi_flash_io0_o : out STD_LOGIC;
+    qspi_flash_io0_t : out STD_LOGIC;
+    qspi_flash_io1_i : in STD_LOGIC;
+    qspi_flash_io1_o : out STD_LOGIC;
+    qspi_flash_io1_t : out STD_LOGIC;
+    qspi_flash_io2_i : in STD_LOGIC;
+    qspi_flash_io2_o : out STD_LOGIC;
+    qspi_flash_io2_t : out STD_LOGIC;
+    qspi_flash_io3_i : in STD_LOGIC;
+    qspi_flash_io3_o : out STD_LOGIC;
+    qspi_flash_io3_t : out STD_LOGIC;
+    qspi_flash_sck_i : in STD_LOGIC;
+    qspi_flash_sck_o : out STD_LOGIC;
+    qspi_flash_sck_t : out STD_LOGIC;
+    qspi_flash_ss_i : in STD_LOGIC;
+    qspi_flash_ss_o : out STD_LOGIC;
+    qspi_flash_ss_t : out STD_LOGIC
+  );
+  end component system_100;
+  component IOBUF is
+  port (
+    I : in STD_LOGIC;
+    O : out STD_LOGIC;
+    T : in STD_LOGIC;
+    IO : inout STD_LOGIC
+  );
+  end component IOBUF;
+  signal eth_mdio_mdc_mdio_i : STD_LOGIC;
+  signal eth_mdio_mdc_mdio_o : STD_LOGIC;
+  signal eth_mdio_mdc_mdio_t : STD_LOGIC;
+  signal ja_pin10_i : STD_LOGIC;
+  signal ja_pin10_o : STD_LOGIC;
+  signal ja_pin10_t : STD_LOGIC;
+  signal ja_pin1_i : STD_LOGIC;
+  signal ja_pin1_o : STD_LOGIC;
+  signal ja_pin1_t : STD_LOGIC;
+  signal ja_pin2_i : STD_LOGIC;
+  signal ja_pin2_o : STD_LOGIC;
+  signal ja_pin2_t : STD_LOGIC;
+  signal ja_pin3_i : STD_LOGIC;
+  signal ja_pin3_o : STD_LOGIC;
+  signal ja_pin3_t : STD_LOGIC;
+  signal ja_pin4_i : STD_LOGIC;
+  signal ja_pin4_o : STD_LOGIC;
+  signal ja_pin4_t : STD_LOGIC;
+  signal ja_pin7_i : STD_LOGIC;
+  signal ja_pin7_o : STD_LOGIC;
+  signal ja_pin7_t : STD_LOGIC;
+  signal ja_pin8_i : STD_LOGIC;
+  signal ja_pin8_o : STD_LOGIC;
+  signal ja_pin8_t : STD_LOGIC;
+  signal ja_pin9_i : STD_LOGIC;
+  signal ja_pin9_o : STD_LOGIC;
+  signal ja_pin9_t : STD_LOGIC;
+  signal jb_pin10_i : STD_LOGIC;
+  signal jb_pin10_o : STD_LOGIC;
+  signal jb_pin10_t : STD_LOGIC;
+  signal jb_pin1_i : STD_LOGIC;
+  signal jb_pin1_o : STD_LOGIC;
+  signal jb_pin1_t : STD_LOGIC;
+  signal jb_pin2_i : STD_LOGIC;
+  signal jb_pin2_o : STD_LOGIC;
+  signal jb_pin2_t : STD_LOGIC;
+  signal jb_pin3_i : STD_LOGIC;
+  signal jb_pin3_o : STD_LOGIC;
+  signal jb_pin3_t : STD_LOGIC;
+  signal jb_pin4_i : STD_LOGIC;
+  signal jb_pin4_o : STD_LOGIC;
+  signal jb_pin4_t : STD_LOGIC;
+  signal jb_pin7_i : STD_LOGIC;
+  signal jb_pin7_o : STD_LOGIC;
+  signal jb_pin7_t : STD_LOGIC;
+  signal jb_pin8_i : STD_LOGIC;
+  signal jb_pin8_o : STD_LOGIC;
+  signal jb_pin8_t : STD_LOGIC;
+  signal jb_pin9_i : STD_LOGIC;
+  signal jb_pin9_o : STD_LOGIC;
+  signal jb_pin9_t : STD_LOGIC;
+  signal qspi_flash_io0_i : STD_LOGIC;
+  signal qspi_flash_io0_o : STD_LOGIC;
+  signal qspi_flash_io0_t : STD_LOGIC;
+  signal qspi_flash_io1_i : STD_LOGIC;
+  signal qspi_flash_io1_o : STD_LOGIC;
+  signal qspi_flash_io1_t : STD_LOGIC;
+  signal qspi_flash_io2_i : STD_LOGIC;
+  signal qspi_flash_io2_o : STD_LOGIC;
+  signal qspi_flash_io2_t : STD_LOGIC;
+  signal qspi_flash_io3_i : STD_LOGIC;
+  signal qspi_flash_io3_o : STD_LOGIC;
+  signal qspi_flash_io3_t : STD_LOGIC;
+  signal qspi_flash_sck_i : STD_LOGIC;
+  signal qspi_flash_sck_o : STD_LOGIC;
+  signal qspi_flash_sck_t : STD_LOGIC;
+  signal qspi_flash_ss_i : STD_LOGIC;
+  signal qspi_flash_ss_o : STD_LOGIC;
+  signal qspi_flash_ss_t : STD_LOGIC;
+begin
+eth_mdio_mdc_mdio_iobuf: component IOBUF
+     port map (
+      I => eth_mdio_mdc_mdio_o,
+      IO => eth_mdio_mdc_mdio_io,
+      O => eth_mdio_mdc_mdio_i,
+      T => eth_mdio_mdc_mdio_t
+    );
+ja_pin10_iobuf: component IOBUF
+     port map (
+      I => ja_pin10_o,
+      IO => ja_pin10_io,
+      O => ja_pin10_i,
+      T => ja_pin10_t
+    );
+ja_pin1_iobuf: component IOBUF
+     port map (
+      I => ja_pin1_o,
+      IO => ja_pin1_io,
+      O => ja_pin1_i,
+      T => ja_pin1_t
+    );
+ja_pin2_iobuf: component IOBUF
+     port map (
+      I => ja_pin2_o,
+      IO => ja_pin2_io,
+      O => ja_pin2_i,
+      T => ja_pin2_t
+    );
+ja_pin3_iobuf: component IOBUF
+     port map (
+      I => ja_pin3_o,
+      IO => ja_pin3_io,
+      O => ja_pin3_i,
+      T => ja_pin3_t
+    );
+ja_pin4_iobuf: component IOBUF
+     port map (
+      I => ja_pin4_o,
+      IO => ja_pin4_io,
+      O => ja_pin4_i,
+      T => ja_pin4_t
+    );
+ja_pin7_iobuf: component IOBUF
+     port map (
+      I => ja_pin7_o,
+      IO => ja_pin7_io,
+      O => ja_pin7_i,
+      T => ja_pin7_t
+    );
+ja_pin8_iobuf: component IOBUF
+     port map (
+      I => ja_pin8_o,
+      IO => ja_pin8_io,
+      O => ja_pin8_i,
+      T => ja_pin8_t
+    );
+ja_pin9_iobuf: component IOBUF
+     port map (
+      I => ja_pin9_o,
+      IO => ja_pin9_io,
+      O => ja_pin9_i,
+      T => ja_pin9_t
+    );
+jb_pin10_iobuf: component IOBUF
+     port map (
+      I => jb_pin10_o,
+      IO => jb_pin10_io,
+      O => jb_pin10_i,
+      T => jb_pin10_t
+    );
+jb_pin1_iobuf: component IOBUF
+     port map (
+      I => jb_pin1_o,
+      IO => jb_pin1_io,
+      O => jb_pin1_i,
+      T => jb_pin1_t
+    );
+jb_pin2_iobuf: component IOBUF
+     port map (
+      I => jb_pin2_o,
+      IO => jb_pin2_io,
+      O => jb_pin2_i,
+      T => jb_pin2_t
+    );
+jb_pin3_iobuf: component IOBUF
+     port map (
+      I => jb_pin3_o,
+      IO => jb_pin3_io,
+      O => jb_pin3_i,
+      T => jb_pin3_t
+    );
+jb_pin4_iobuf: component IOBUF
+     port map (
+      I => jb_pin4_o,
+      IO => jb_pin4_io,
+      O => jb_pin4_i,
+      T => jb_pin4_t
+    );
+jb_pin7_iobuf: component IOBUF
+     port map (
+      I => jb_pin7_o,
+      IO => jb_pin7_io,
+      O => jb_pin7_i,
+      T => jb_pin7_t
+    );
+jb_pin8_iobuf: component IOBUF
+     port map (
+      I => jb_pin8_o,
+      IO => jb_pin8_io,
+      O => jb_pin8_i,
+      T => jb_pin8_t
+    );
+jb_pin9_iobuf: component IOBUF
+     port map (
+      I => jb_pin9_o,
+      IO => jb_pin9_io,
+      O => jb_pin9_i,
+      T => jb_pin9_t
+    );
+qspi_flash_io0_iobuf: component IOBUF
+     port map (
+      I => qspi_flash_io0_o,
+      IO => qspi_flash_io0_io,
+      O => qspi_flash_io0_i,
+      T => qspi_flash_io0_t
+    );
+qspi_flash_io1_iobuf: component IOBUF
+     port map (
+      I => qspi_flash_io1_o,
+      IO => qspi_flash_io1_io,
+      O => qspi_flash_io1_i,
+      T => qspi_flash_io1_t
+    );
+qspi_flash_io2_iobuf: component IOBUF
+     port map (
+      I => qspi_flash_io2_o,
+      IO => qspi_flash_io2_io,
+      O => qspi_flash_io2_i,
+      T => qspi_flash_io2_t
+    );
+qspi_flash_io3_iobuf: component IOBUF
+     port map (
+      I => qspi_flash_io3_o,
+      IO => qspi_flash_io3_io,
+      O => qspi_flash_io3_i,
+      T => qspi_flash_io3_t
+    );
+qspi_flash_sck_iobuf: component IOBUF
+     port map (
+      I => qspi_flash_sck_o,
+      IO => qspi_flash_sck_io,
+      O => qspi_flash_sck_i,
+      T => qspi_flash_sck_t
+    );
+qspi_flash_ss_iobuf: component IOBUF
+     port map (
+      I => qspi_flash_ss_o,
+      IO => qspi_flash_ss_io,
+      O => qspi_flash_ss_i,
+      T => qspi_flash_ss_t
+    );
+system_100_i: component system_100
+     port map (
+      ddr3_sdram_addr(13 downto 0) => ddr3_sdram_addr(13 downto 0),
+      ddr3_sdram_ba(2 downto 0) => ddr3_sdram_ba(2 downto 0),
+      ddr3_sdram_cas_n => ddr3_sdram_cas_n,
+      ddr3_sdram_ck_n(0) => ddr3_sdram_ck_n(0),
+      ddr3_sdram_ck_p(0) => ddr3_sdram_ck_p(0),
+      ddr3_sdram_cke(0) => ddr3_sdram_cke(0),
+      ddr3_sdram_cs_n(0) => ddr3_sdram_cs_n(0),
+      ddr3_sdram_dm(1 downto 0) => ddr3_sdram_dm(1 downto 0),
+      ddr3_sdram_dq(15 downto 0) => ddr3_sdram_dq(15 downto 0),
+      ddr3_sdram_dqs_n(1 downto 0) => ddr3_sdram_dqs_n(1 downto 0),
+      ddr3_sdram_dqs_p(1 downto 0) => ddr3_sdram_dqs_p(1 downto 0),
+      ddr3_sdram_odt(0) => ddr3_sdram_odt(0),
+      ddr3_sdram_ras_n => ddr3_sdram_ras_n,
+      ddr3_sdram_reset_n => ddr3_sdram_reset_n,
+      ddr3_sdram_we_n => ddr3_sdram_we_n,
+      dip_switches_4bits_tri_i(3 downto 0) => dip_switches_4bits_tri_i(3 downto 0),
+      eth_mdio_mdc_mdc => eth_mdio_mdc_mdc,
+      eth_mdio_mdc_mdio_i => eth_mdio_mdc_mdio_i,
+      eth_mdio_mdc_mdio_o => eth_mdio_mdc_mdio_o,
+      eth_mdio_mdc_mdio_t => eth_mdio_mdc_mdio_t,
+      eth_mii_col => eth_mii_col,
+      eth_mii_crs => eth_mii_crs,
+      eth_mii_rst_n => eth_mii_rst_n,
+      eth_mii_rx_clk => eth_mii_rx_clk,
+      eth_mii_rx_dv => eth_mii_rx_dv,
+      eth_mii_rx_er => eth_mii_rx_er,
+      eth_mii_rxd(3 downto 0) => eth_mii_rxd(3 downto 0),
+      eth_mii_tx_clk => eth_mii_tx_clk,
+      eth_mii_tx_en => eth_mii_tx_en,
+      eth_mii_txd(3 downto 0) => eth_mii_txd(3 downto 0),
+      eth_ref_clk => eth_ref_clk,
+      ja_pin10_i => ja_pin10_i,
+      ja_pin10_o => ja_pin10_o,
+      ja_pin10_t => ja_pin10_t,
+      ja_pin1_i => ja_pin1_i,
+      ja_pin1_o => ja_pin1_o,
+      ja_pin1_t => ja_pin1_t,
+      ja_pin2_i => ja_pin2_i,
+      ja_pin2_o => ja_pin2_o,
+      ja_pin2_t => ja_pin2_t,
+      ja_pin3_i => ja_pin3_i,
+      ja_pin3_o => ja_pin3_o,
+      ja_pin3_t => ja_pin3_t,
+      ja_pin4_i => ja_pin4_i,
+      ja_pin4_o => ja_pin4_o,
+      ja_pin4_t => ja_pin4_t,
+      ja_pin7_i => ja_pin7_i,
+      ja_pin7_o => ja_pin7_o,
+      ja_pin7_t => ja_pin7_t,
+      ja_pin8_i => ja_pin8_i,
+      ja_pin8_o => ja_pin8_o,
+      ja_pin8_t => ja_pin8_t,
+      ja_pin9_i => ja_pin9_i,
+      ja_pin9_o => ja_pin9_o,
+      ja_pin9_t => ja_pin9_t,
+      jb_pin10_i => jb_pin10_i,
+      jb_pin10_o => jb_pin10_o,
+      jb_pin10_t => jb_pin10_t,
+      jb_pin1_i => jb_pin1_i,
+      jb_pin1_o => jb_pin1_o,
+      jb_pin1_t => jb_pin1_t,
+      jb_pin2_i => jb_pin2_i,
+      jb_pin2_o => jb_pin2_o,
+      jb_pin2_t => jb_pin2_t,
+      jb_pin3_i => jb_pin3_i,
+      jb_pin3_o => jb_pin3_o,
+      jb_pin3_t => jb_pin3_t,
+      jb_pin4_i => jb_pin4_i,
+      jb_pin4_o => jb_pin4_o,
+      jb_pin4_t => jb_pin4_t,
+      jb_pin7_i => jb_pin7_i,
+      jb_pin7_o => jb_pin7_o,
+      jb_pin7_t => jb_pin7_t,
+      jb_pin8_i => jb_pin8_i,
+      jb_pin8_o => jb_pin8_o,
+      jb_pin8_t => jb_pin8_t,
+      jb_pin9_i => jb_pin9_i,
+      jb_pin9_o => jb_pin9_o,
+      jb_pin9_t => jb_pin9_t,
+      led03_rgb(11 downto 0) => led03_rgb(11 downto 0),
+      led47(3 downto 0) => led47(3 downto 0),
+      push_buttons_4bits_tri_i(3 downto 0) => push_buttons_4bits_tri_i(3 downto 0),
+      qspi_flash_io0_i => qspi_flash_io0_i,
+      qspi_flash_io0_o => qspi_flash_io0_o,
+      qspi_flash_io0_t => qspi_flash_io0_t,
+      qspi_flash_io1_i => qspi_flash_io1_i,
+      qspi_flash_io1_o => qspi_flash_io1_o,
+      qspi_flash_io1_t => qspi_flash_io1_t,
+      qspi_flash_io2_i => qspi_flash_io2_i,
+      qspi_flash_io2_o => qspi_flash_io2_o,
+      qspi_flash_io2_t => qspi_flash_io2_t,
+      qspi_flash_io3_i => qspi_flash_io3_i,
+      qspi_flash_io3_o => qspi_flash_io3_o,
+      qspi_flash_io3_t => qspi_flash_io3_t,
+      qspi_flash_sck_i => qspi_flash_sck_i,
+      qspi_flash_sck_o => qspi_flash_sck_o,
+      qspi_flash_sck_t => qspi_flash_sck_t,
+      qspi_flash_ss_i => qspi_flash_ss_i,
+      qspi_flash_ss_o => qspi_flash_ss_o,
+      qspi_flash_ss_t => qspi_flash_ss_t,
+      reset => reset,
+      sys_clock => sys_clock,
+      usb_uart_rxd => usb_uart_rxd,
+      usb_uart_txd => usb_uart_txd
+    );
+end STRUCTURE;
